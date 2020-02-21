5ffc18679acfbbc296da30512aa0edcde26bec1f
maxrusmr2011@gmail.com <maxrusmr2011@gmail.com>
2020-02-21 23:27:49 +0200

    init: start towel-sort task

---
+++ src/index.js
@@ -0,0 +1,12 @@
+module.exports = function towelSort(matrix) {
+    let newArr = [];
+    if (matrix) {
+        matrix.forEach((item, i) => {
+            if (i % 2) {
+                item.reverse();
+            }
+            newArr = newArr.concat(item);
+        });
+    }
+    return newArr;
+}
